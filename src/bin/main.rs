65da7d0ddf247f32b921c0b2743550bd038f355a
duyquang6 <nguyenduyquang06@gmail.com>
2020-09-18 19:12:39 +0700

    init repo

---
+++ src/bin/main.rs
@@ -0,0 +1,20 @@
+use std::thread;
+use std::time::Duration;
+use ::rust_thread_pool::ThreadPool;
+fn main() {
+
+    let pool = ThreadPool::new(4);
+
+    for i in 1..10 {        
+        pool.execute(move || {
+            do_task(i);
+        });
+    }
+
+    
+}
+
+fn do_task(i: i32) {
+    thread::sleep(Duration::from_secs(2));
+    println!("task {} done", i)
+}
